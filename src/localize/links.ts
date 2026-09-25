2c23081b8e9a229351fbf7a8393ea143d6a6b09a
alxl <its.alxl@proton.me>
2026-09-25 00:52:41 -0500

    add about section

--- src/localize/links.ts
+++ src/localize/links.ts
@@ -2,4 +2,5 @@ export default {
 	alxl: "https://itsalxl.com",
 	repo: "https://github.com/ItsAlxl/gobbo-datacron",
 	donate: "https://ko-fi.com/itsalxl",
+	localstorage: "https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API",
 }
